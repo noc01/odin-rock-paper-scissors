bd098f0e478c6b3f2a1db0614b5de58334702bda
Carlos <140348406+noc01@users.noreply.github.com>
2023-08-15 20:03:36 -0500

    create winner

--- javascript.js
+++ javascript.js
@@ -10,3 +10,23 @@ function getComputerChoice() {
     }
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    let winner;
+    if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+        winner = "You Lose! Paper beats Rock";
+    } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
+        winner = "You Lose! Scissors beats Paper";
+    } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
+        winner = "You Lose! Rock beats Scissors";
+    } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+        winner = "You Win! Paper beats Rock";
+    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+        winner = "You Win! Scissors beats Paper";
+    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+        winner = "You Win! Rock beats Scissors";
+    } else {
+        winner = "Tie";
+    }
+    return winner;
+}
