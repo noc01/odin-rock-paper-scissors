a3a1444ff67bd87d8799c9acfdb3077bf740b38f
Carlos <140348406+noc01@users.noreply.github.com>
2023-08-16 19:28:27 -0500

    create comparison and function of 5 games

--- javascript.js
+++ javascript.js
@@ -14,19 +14,79 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     let winner;
     if (playerSelection == "ROCK" && computerSelection == "PAPER") {
-        winner = "You Lose! Paper beats Rock";
+        winner = computerSelection;
+        console.log("You Lose! Paper beats Rock");
     } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
-        winner = "You Lose! Scissors beats Paper";
+        winner = computerSelection;
+        console.log("You Lose! Scissors beats Paper");
     } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
-        winner = "You Lose! Rock beats Scissors";
+        winner = computerSelection;
+        console.log("You Lose! Rock beats Scissors");
     } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
-        winner = "You Win! Paper beats Rock";
+        winner = playerSelection;
+        console.log("You Win! Paper beats Rock");
     } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
-        winner = "You Win! Scissors beats Paper";
+        winner = playerSelection;
+        console.log("You Win! Scissors beats Paper");
     } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
-        winner = "You Win! Rock beats Scissors";
-    } else {
-        winner = "Tie";
+        winner = playerSelection;
+        console.log("You Win! Rock beats Scissors");
+    } else if (playerSelection == computerSelection) {
+        console.log("Tie!");
     }
     return winner;
 }
+function game() {
+    let scorePlayer;
+    let scoreComputer;
+    let playerSelection = prompt("Rock Paper Scissors: ").toUpperCase();
+    let computerSelection =  getComputerChoice().toUpperCase();
+    let winner = playRound(playerSelection, computerSelection);
+    if (winner == playerSelection) {
+        scorePlayer += 1;
+    } else if (winner == computerSelection) {
+        scoreComputer += 1;
+    }
+    playerSelection = prompt("Rock Paper Scissors: ").toUpperCase();
+    computerSelection =  getComputerChoice().toUpperCase();
+    winner = playRound(playerSelection, computerSelection);
+    if (winner == playerSelection) {
+        scorePlayer += 1;
+    } else if (winner == computerSelection) {
+        scoreComputer += 1;
+    }
+    playerSelection = prompt("Rock Paper Scissors: ").toUpperCase();
+    computerSelection =  getComputerChoice().toUpperCase();
+    winner = playRound(playerSelection, computerSelection);
+    if (winner == playerSelection) {
+        scorePlayer += 1;
+    } else if (winner == computerSelection) {
+        scoreComputer += 1;
+    }
+    playerSelection = prompt("Rock Paper Scissors: ").toUpperCase();
+    computerSelection =  getComputerChoice().toUpperCase();
+    winner = playRound(playerSelection, computerSelection);
+    if (winner == playerSelection) {
+        scorePlayer += 1;
+    } else if (winner == computerSelection) {
+        scoreComputer += 1;
+    }
+    playerSelection = prompt("Rock Paper Scissors: ").toUpperCase();
+    computerSelection =  getComputerChoice().toUpperCase();
+    winner = playRound(playerSelection, computerSelection);
+    if (winner == playerSelection) {
+        scorePlayer += 1;
+    } else if (winner == computerSelection) {
+        scoreComputer += 1;
+    }
+    
+    if (scorePlayer > scoreComputer) {
+        console.log(`You Win! ${scorePlayer} - ${scoreComputer}`);
+    } else if (scorePlayer < scoreComputer) {
+        console.log(`You Lose! ${scorePlayer} - ${scoreComputer}`);
+    } else {
+        console.log(`Tie! ${scorePlayer} - ${scoreComputer}`)
+    }
+}
+
+game();
